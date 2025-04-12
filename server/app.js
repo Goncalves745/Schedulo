afd720d0c56c955d590e0ef080396b205c863185
Diogo <110359163+Goncalves745@users.noreply.github.com>
2025-04-12 18:56:22 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,8 @@
-// app.js
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./swagger"); // ficheiro que criaste
+const swaggerSpec = require("./swagger");
 
 const authRoutes = require("./routes/auth");
 const businessRoutes = require("./routes/business");
@@ -14,12 +13,22 @@ const publicRoutes = require("./routes/public");
 dotenv.config();
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = [
+  "https://schedulo-omega.vercel.app",
+  "http://localhost:5173",
+];
+
 app.use(cors({
-  origin: "https://schedulo-omega.vercel.app", 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true,
 }));
 
+app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/auth", authRoutes);
 app.use("/api/business", businessRoutes);
