171633c20d04bd050b51b415c81b51eac20127c7
Diogo <110359163+Goncalves745@users.noreply.github.com>
2025-04-12 18:35:37 +0100

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://schedulo-production.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
 export default Api;
