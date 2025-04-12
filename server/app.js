156ec87f2c3320ffe6d9ae2cdfc0ad6130a2ecc9
Diogo <110359163+Goncalves745@users.noreply.github.com>
2025-04-12 18:18:04 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,6 +16,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: "https://schedulo-omega.vercel.app/", 
+}));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/auth", authRoutes);
