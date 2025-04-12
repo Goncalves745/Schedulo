da8187270cb098b9ee11c5400528b229aa5c7a8f
Diogo <110359163+Goncalves745@users.noreply.github.com>
2025-04-12 17:49:44 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 
 if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Servidor a correr em http://localhost:${PORT}`);
   });
